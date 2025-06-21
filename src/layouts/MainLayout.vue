526866b03b4323ab3d815bd21aed2d450be81157
ElizabethHuang <lizne6z0@gmail.com>
2025-06-21 12:39:24 +0800

    feat: Modify header.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,18 +10,15 @@
           Souflair
         </q-toolbar-title>
         <div class="row q-gutter-xs q-pt-sm q-pr-sm">
-          <q-btn flat label="登入" />
-          <q-btn flat label="登出" />
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/avatar.png">
-          </q-avatar>
+          <template v-if="logined">
+            <q-btn @click="logout" flat label="登出" />
+            <q-avatar>
+              <img src="~assets/account_circle.svg">
+            </q-avatar>
+          </template>
+          <q-btn v-else to="/login" flat label="登入" />
         </div>
       </q-toolbar>
-
-      <q-tabs align="left">
-        <q-route-tab to="/" label="首頁" />
-        <q-route-tab to="/actors" label="演員" />
-      </q-tabs>
     </q-header>
 
     <q-page-container>
@@ -31,7 +28,7 @@
     <q-footer class="text-white" id="footer">
       <q-toolbar>
         <span>
-          © 2025 Souflair. All rights reserved.
+          © 2025 Elizabeth Huang. All rights reserved.
         </span>
       </q-toolbar>
     </q-footer>
@@ -39,14 +36,35 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref } from 'vue';
+import { Notify } from 'quasar';
+import { api } from 'boot/axios';
+import { useRouter } from 'vue-router';
 
 export default {
   setup() {
     const leftDrawerOpen = ref(false);
+    const router = useRouter();
+    const logined = ref(document.cookie.indexOf('login=') !== -1);
+    async function logout() {
+      try {
+        const response = await api.post(
+          '/api/logout',
+          {},
+          { withCredentials: true }
+        );
+        logined.value = false;
+        Notify.create(response.data.message);
+        router.push({ path: '/' });
+      } catch (error) {
+        Notify.create(error.response.data.message);
+      }
+    }
 
     return {
       leftDrawerOpen,
+      logined,
+      logout,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value;
       },
